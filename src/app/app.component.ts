4551d72cdba2b25c76c36b445d8bba3f36d77582
jaydeep7 <jaydeep7karena7@gmail.com>
2018-01-29 22:49:08 +0530

    some random commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Article Dictionary';
+  wordsInArticle: string[] = [];
+  @Input('article') article: any = '';
+
+  constructor() {
+    this.article = '';
+  }
+
+  findMeanings(articleText: string) {
+    if (articleText.length === 0) {
+      this.wordsInArticle = [];
+      return;
+    }
+    console.log(articleText.length);
+    this.wordsInArticle = articleText
+      .match(new RegExp(/[a-zA-Z ]/g)) // filtering special characters
+      .join('') // joining each character
+      .split(' '); // converting to array of words
+
+    // filtering blank array element
+    this.wordsInArticle = this.wordsInArticle.filter((n) => n !== '');
+
+    // removing duplicate elements
+    this.wordsInArticle = this.wordsInArticle
+      .filter((v, i) => this.wordsInArticle.indexOf(v) === i);
+
+    // sort the array
+    // this.wordsInArticle = this.wordsInArticle.sort();
+    console.log(this.wordsInArticle);
+
+    // try this API
+    // https://github.com/matheuss/google-translate-api
+
+  }
 }
